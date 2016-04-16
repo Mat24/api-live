e88b0de443320c25e8a818248cf6849d0f8e42b1
Mat24 <smat_24@hotmail.com>
2016-04-16 09:07:05 -0500

    Add set_user callback

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,18 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show,:update,:destroy]
+
   def index
     @users = User.all
     #render json: users, status: :ok
   end
 
   def show
-    @user = User.find(params[:id])
     #render json: user, status: :ok
   end
 
   # creamos un nuevo usuario
-  def createe
+  def create
     user = User.new(user_params)
     if user.save
       render json: {status: "Se creo el usuario"}, status: :ok
@@ -21,12 +22,23 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    if user.update(user_params)
+    if @user.update(user_params)
       render json: { status: "Usuario actualizado con exito" }
     else
-      render json: { status: "mateo" }, status: :ok
+      render json: { status: "Error al actualizar cuenta" }, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    if @user.destroy
+      render json: { status: "Usuario eliminado con exito" }, status: :ok
+    else
+      render json: { status: "Error al eliminar el usuario" }, status: :unprocessable_entity
+    end
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
   def user_params
